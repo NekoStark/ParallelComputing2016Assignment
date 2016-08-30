bd04c50ae6dd3b7f94a3a112e75dd8bc80230036
stark <simone.mattolini@gmail.com>
2016-08-30 11:13:00 +0200

    commit

--- word-count/src/main/java/it/unifi/ing/pc/wordcount/WordCount.java
+++ word-count/src/main/java/it/unifi/ing/pc/wordcount/WordCount.java
@@ -1,46 +1,35 @@
 package it.unifi.ing.pc.wordcount;
 
-import static it.unifi.ing.pc.utilities.ResourceLoader.asURI;
-
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import it.unifi.ing.pc.wordcount.config.Config;
 import it.unifi.ing.pc.wordcount.elephantdb.DatabaseOperation;
 import it.unifi.ing.pc.wordcount.elephantdb.EndpointFactory;
-import it.unifi.ing.pc.wordcount.mapreduce.Map;
-import it.unifi.ing.pc.wordcount.mapreduce.Reduce;
 
 public class WordCount {
 
 	public static void main(String[] args) throws Exception {
-		clearOutputFolder();
-
-		Configuration conf = new Configuration();
-
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(WordCount.class);
-
-		job.setMapperClass(Map.class);
-		job.setCombinerClass(Reduce.class);
-		job.setReducerClass(Reduce.class);
-
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
-
-		FileInputFormat.addInputPath(job, new Path(asURI("/input/lorem1.txt")));
-		FileInputFormat.addInputPath(job, new Path(asURI("/input/lorem2.txt")));
-		FileOutputFormat.setOutputPath(job, new Path(Config.OUTPUT_DIR));
-
-		job.waitForCompletion(true);
+//		clearOutputFolder();
+//
+//		Configuration conf = new Configuration();
+//
+//		Job job = Job.getInstance(conf, "word count");
+//		job.setJarByClass(WordCount.class);
+//
+//		job.setMapperClass(Map.class);
+//		job.setCombinerClass(Reduce.class);
+//		job.setReducerClass(Reduce.class);
+//
+//		job.setOutputKeyClass(Text.class);
+//		job.setOutputValueClass(IntWritable.class);
+//
+//		FileInputFormat.addInputPath(job, new Path(asURI("/input/lorem1.txt")));
+//		FileInputFormat.addInputPath(job, new Path(asURI("/input/lorem2.txt")));
+//		FileOutputFormat.setOutputPath(job, new Path(Config.OUTPUT_DIR));
+//
+//		job.waitForCompletion(true);
 
 		DatabaseOperation.writeToDb( EndpointFactory.getHfs() );
 		
--- word-count/src/main/java/it/unifi/ing/pc/wordcount/elephantdb/EndpointFactory.java
+++ word-count/src/main/java/it/unifi/ing/pc/wordcount/elephantdb/EndpointFactory.java
@@ -1,22 +1,22 @@
 package it.unifi.ing.pc.wordcount.elephantdb;
 
 import cascading.scheme.hadoop.TextLine;
+import cascading.tap.hadoop.Dfs;
 import cascading.tap.hadoop.Hfs;
 import elephantdb.DomainSpec;
 import elephantdb.jcascalog.EDB;
 import elephantdb.partition.HashModScheme;
 import elephantdb.persistence.JavaBerkDB;
-import it.unifi.ing.pc.wordcount.config.Config;
 
 public class EndpointFactory {
 
 	public static Hfs getHfs() {
-		return new Hfs(new TextLine(), Config.OUTPUT_DIR + "/part-r-00000");
+		return new Dfs(new TextLine(), "hdfs://localhost:54310/output/part-r-00000");
 	}
 	
 	public static Object getEdb() {
 		DomainSpec spec = new DomainSpec(new JavaBerkDB(), new HashModScheme(), 1);
-		return EDB.makeKeyValTap(Config.OUTPUT_DIR+"/eleph", spec);
+		return EDB.makeKeyValTap("hdfs://localhost:54310/output/eleph", spec);
 	}
 	
 }
