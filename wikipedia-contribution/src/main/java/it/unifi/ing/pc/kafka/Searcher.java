22e9900ecffdb423864ac7f9b15b78c5f1e02eb7
tom3mi <tommaso@man-super.com>
2016-09-01 10:35:46 +0200

    add topic creation

--- wikipedia-contribution/src/main/java/it/unifi/ing/pc/kafka/Searcher.java
+++ wikipedia-contribution/src/main/java/it/unifi/ing/pc/kafka/Searcher.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Properties;
 import java.util.Set;
 
+import org.I0Itec.zkclient.ZkClient;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -13,6 +14,8 @@ import it.unifi.ing.pc.searcher.FlickrSearcher;
 import it.unifi.ing.pc.searcher.GoogleImageSearcher;
 import it.unifi.ing.pc.searcher.ImageFreshness;
 import it.unifi.ing.pc.searcher.Result;
+import kafka.admin.AdminUtils;
+import kafka.utils.ZKStringSerializer$;
 
 public class Searcher {
 
@@ -32,11 +35,14 @@ public class Searcher {
 		props.put("buffer.memory", 33554432);
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+			
+		ZkClient zkClient = new ZkClient("localhost:2181", 10000, 10000, ZKStringSerializer$.MODULE$);
+		AdminUtils.createTopic(zkClient, "my-topic", 10, 1, new Properties());
 		
 		Producer<String, Set<Result>> producer = new KafkaProducer<>(props);
-		producer.send(new ProducerRecord<String,Set<Result>>(args[0], googleImages));
-		producer.send(new ProducerRecord<String,Set<Result>>(args[0], bingImages));	
-		producer.send(new ProducerRecord<String,Set<Result>>(args[0], flickrImages));	
+		producer.send(new ProducerRecord<String,Set<Result>>("my-topic", args[0], googleImages));
+		producer.send(new ProducerRecord<String,Set<Result>>("my-topic", args[0], bingImages));	
+		producer.send(new ProducerRecord<String,Set<Result>>("my-topic", args[0], flickrImages));	
 		
 		producer.close();
 	}
